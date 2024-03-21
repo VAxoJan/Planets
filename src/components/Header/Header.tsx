b1b718a8a7001f500c95365749a29eed9d5daf7a
vaxo <vaxojaniashvili186@gmail.com>
2024-03-22 02:00:20 +0400

    working-json

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+
 const Header = () => {
+    const [dataMap,setDataMap] = useState<any>([])
+    useEffect(() => {
+        const axiosData = async () => {
+            const response = await axios.get("../..data.json")
+            setDataMap(response.data)
+            console.log(dataMap);
+        }
+        axiosData()
+        
+    },[])
   return (
     <header className="flex list-none gap-10 bg-purple-600 w-[300px] justify-center text-white m-auto">
         <li>
