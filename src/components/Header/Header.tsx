41ffed4c7bfa04bba55aa9b5995622907a25c306
vaxo <vaxojaniashvili186@gmail.com>
2024-03-22 21:42:34 +0400

    json-error-fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,50 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const Header: React.FC = () => {
-  const [planets, setPlanets] = useState<any[]>([]);
+const Header:any = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      axios.get("./data.json")
-        .then(response => {
-          setPlanets(response.data);
+      await fetch("/src/components/Header/data.json")
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     };
-    console.log(planets);
-    
+
     fetchData();
   }, []);
 
-  console.log(planets);
-
   return (
-    <div className="flex h-16 bg-purple-600 text-white">
+    <div className="flex h-[85px] bg-[#070724]  text-white m-auto">
       <header className="flex list-none gap-10 w-full justify-end px-10 items-center font-headerFont">
-        {/* {planets.map((planet: any) => (
-          <li key={planet.name}>
-            <Link to={`/${planet.name.toUpperCase()}`}>{planet.name.toUpperCase()}</Link>
-          </li>
-        ))} */}
+        <li>
+          <Link to="/Mercury">MERCURY</Link>
+        </li>
+        <li>
+          <Link to="/Venus">VENUS</Link>
+        </li>
+        <li>
+          <Link to="/Earth">EARTH</Link>
+        </li>
+        <li>
+          <Link to="/Mars">MARS</Link>
+        </li>
+        <li>
+          <Link to="/Jupiter">JUPITER</Link>
+        </li>
+        <li>
+          <Link to="/Saturn">SATURN</Link>
+        </li>
+        <li>
+          <Link to="/Uranus">URANUS</Link>
+        </li>
+        <li>
+          <Link to="/Neptune">NEPTUNE</Link>
+        </li>
       </header>
     </div>
   );
